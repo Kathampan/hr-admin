f4c82ad13ee56d7ff71cc2515407884ccfad6edd
bibingsoft <bibinjoseph@gsoftcomm.net>
2025-04-02 23:04:13 +0530

    state filed update

--- src/components/EmployeeGrid.jsx
+++ src/components/EmployeeGrid.jsx
@@ -339,7 +339,7 @@ const EmployeeGrid = () => {
       </div>
 
       {filteredData.length > rowsPerPage && (
-        <div className="pagination-container mt-3" style={{ 
+        <div className="pagination-container mt-3" style={{
           position: 'sticky',
           bottom: 0,
           backgroundColor: 'white',
@@ -736,7 +736,7 @@ const EmployeeGrid = () => {
                           onChange={handleInputChange}
                         />
                       </div>
-                      <div className="col-md-4">
+                      {/* <div className="col-md-4">
                         <label className="form-label">State <span className="text-danger">*</span></label>
                         <input
                           type="text"
@@ -745,6 +745,56 @@ const EmployeeGrid = () => {
                           value={newEmployee.state}
                           onChange={handleInputChange}
                         />
+                        {errors.state && <div className="invalid-feedback">{errors.state}</div>}
+                      </div> */}
+                      <div className="col-md-4">
+                        <label className="form-label">State <span className="text-danger">*</span></label>
+                        <select
+                          className={`form-select ${errors.state ? 'is-invalid' : ''}`}
+                          name="state"
+                          value={newEmployee.state}
+                          onChange={handleInputChange}
+                        >
+                          <option value="">Select State</option>
+                          {/* States */}
+                          <option value="Andhra Pradesh">Andhra Pradesh</option>
+                          <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                          <option value="Assam">Assam</option>
+                          <option value="Bihar">Bihar</option>
+                          <option value="Chhattisgarh">Chhattisgarh</option>
+                          <option value="Goa">Goa</option>
+                          <option value="Gujarat">Gujarat</option>
+                          <option value="Haryana">Haryana</option>
+                          <option value="Himachal Pradesh">Himachal Pradesh</option>
+                          <option value="Jharkhand">Jharkhand</option>
+                          <option value="Karnataka">Karnataka</option>
+                          <option value="Kerala">Kerala</option>
+                          <option value="Madhya Pradesh">Madhya Pradesh</option>
+                          <option value="Maharashtra">Maharashtra</option>
+                          <option value="Manipur">Manipur</option>
+                          <option value="Meghalaya">Meghalaya</option>
+                          <option value="Mizoram">Mizoram</option>
+                          <option value="Nagaland">Nagaland</option>
+                          <option value="Odisha">Odisha</option>
+                          <option value="Punjab">Punjab</option>
+                          <option value="Rajasthan">Rajasthan</option>
+                          <option value="Sikkim">Sikkim</option>
+                          <option value="Tamil Nadu">Tamil Nadu</option>
+                          <option value="Telangana">Telangana</option>
+                          <option value="Tripura">Tripura</option>
+                          <option value="Uttar Pradesh">Uttar Pradesh</option>
+                          <option value="Uttarakhand">Uttarakhand</option>
+                          <option value="West Bengal">West Bengal</option>
+                          {/* Union Territories */}
+                          <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+                          <option value="Chandigarh">Chandigarh</option>
+                          <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
+                          <option value="Delhi">Delhi</option>
+                          <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+                          <option value="Ladakh">Ladakh</option>
+                          <option value="Lakshadweep">Lakshadweep</option>
+                          <option value="Puducherry">Puducherry</option>
+                        </select>
                         {errors.state && <div className="invalid-feedback">{errors.state}</div>}
                       </div>
                       <div className="col-md-4">
